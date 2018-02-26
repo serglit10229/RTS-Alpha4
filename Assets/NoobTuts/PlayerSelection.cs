0429528090eaeca7b6eae8952e210852c4b94968
Sergei Litovchenko <serglit10229@gmail.com>
2018-02-25 21:48:39 -0800

    Initial commit

---
+++ Assets/NoobTuts/PlayerSelection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSelection : MonoBehaviour {    
+    // Selection Circle
+    public GameObject circle;
+    public GameObject tc;
+
+    // OnSelect is called by the RTS Selection System
+    void OnSelect() {
+        Debug.Log("OnSelect");
+        circle.SetActive(true);
+        tc.GetComponent<Count>().selectedUnits++;
+    }
+    
+    // OnDeselect is called by the RTS Selection System
+    void OnDeselect() {
+        Debug.Log("OnDeselect");
+        circle.SetActive(false);
+        tc.GetComponent<Count>().selectedUnits--;
+    }
+}
---
+++ Assets/UnitSpawner.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class UnitSpawner : MonoBehaviour {
+    // unit prefab
+    public GameObject unit;
+    public float spawnRange = 1.5f;
+
+    public float x = 0.075f;
+    public float y = 0.5f;
+    public float z = -0.1f;
+    //public float angle = Random.Range(0.0f, 0.0f);
+    public float speed1 = 0.2f;
+    public float speed2 = 0.2f;
+
+    public bool allowSpawn = false;
+    float prodTime = 5f;
+    public int unitAmount = 0;
+
+    void Update()
+    {
+        for(int i; i > 0)
+        { 
+            prodTime -= Time.deltaTime;
+            if (prodTime < 0)
+            {
+                SpawnUnit();
+                unitAmount--;
+            }
+        }
+        
+    }
+
+    public void SpawnUnit()
+    {
+        
+        // start new animation
+        //GetComponent<PlayCurve>().Toggle();
+
+        // create a new unit at some random position around this place
+        Vector3 pos = transform.position;
+
+        Instantiate(unit, new Vector3(x + pos.x, y + pos.y, z + pos.z), Quaternion.Euler(0.0f, 0.0f, 0.0f));
+        unit.GetComponent<NavMeshAgent>().enabled = false;
+
+        unit.GetComponent<NavMeshAgent>().enabled = true;
+        
+        
+    }
+}
