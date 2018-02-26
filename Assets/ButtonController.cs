9a9468ae923685ec950b7f0fb9f355bbd032a70d
serglit1650 <direct.serglit@gmail.com>
2018-02-26 15:30:49 -0800

    Buttons

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+	GameObject Bot1;
+	GameObject Bot2;
+	GameObject Bot3;
+	GameObject Bot4;
+	GameObject Bot5;
+	GameObject Bot6;
+	GameObject Bot7;
+	GameObject Bot8;
+	GameObject Bot9;
+	GameObject Bot10;
+
+	public bool BotFactoryT1 = false;
+	public bool TankFactoryT1 = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	public void ChangeButton() {
+		
+	}
+}
--- Assets/NoobTuts/PlayerSelection.cs
+++ Assets/NoobTuts/PlayerSelection.cs
@@ -4,19 +4,48 @@ using System.Collections;
 public class PlayerSelection : MonoBehaviour {    
     // Selection Circle
     public GameObject circle;
-    public GameObject tc;
+    //public GameObject tc;
+
+	public bool BotFactoryT1 = false;
+	public bool TankFactoryT1 = false;
+
+	public GameObject BotT1UI;
+	public GameObject TankT1UI;
+
+	void Update()
+	{
+
+	}
 
     // OnSelect is called by the RTS Selection System
     void OnSelect() {
         Debug.Log("OnSelect");
         circle.SetActive(true);
-        tc.GetComponent<Count>().selectedUnits++;
+        //tc.GetComponent<Count>().selectedUnits++;
+
+		if (BotFactoryT1 == true && !BotT1UI.activeSelf) 
+		{
+			BotT1UI.SetActive(true);
+		}
+		if (TankFactoryT1 == true && !TankT1UI.activeSelf) 
+		{
+			TankT1UI.SetActive(true);
+		}
     }
     
     // OnDeselect is called by the RTS Selection System
     void OnDeselect() {
         Debug.Log("OnDeselect");
         circle.SetActive(false);
-        tc.GetComponent<Count>().selectedUnits--;
+        //tc.GetComponent<Count>().selectedUnits--;
+
+		if (BotFactoryT1 == true && BotT1UI.activeSelf) 
+		{
+			BotT1UI.SetActive(false);
+		}
+		if (TankFactoryT1 == true && !TankT1UI.activeSelf) 
+		{
+			TankT1UI.SetActive(true);
+		}
     }
 }
